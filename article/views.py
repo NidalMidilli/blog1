f423f6c0f42da30b941680e127cfb212ec958620
Nydle <nidalmidilli@gmail.com>
2021-08-06 16:02:24 +0300

    first commit

---
+++ article/views.py
@@ -0,0 +1,68 @@
+from blog.settings import TEMPLATES
+from django.shortcuts import render,HttpResponse,redirect,get_object_or_404
+from .forms import ArticleForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Article
+# Create your views here.
+
+def articles(request):
+    articles = Article.objects.all()
+
+    return render(request,"articles.html",{"articles":articles})
+def index(request):
+    context = {       
+    }
+    return render(request,"index.html",context)
+
+def about(request):
+    return render(request,"about.html")
+@login_required(login_url= "user:login")
+def dashboard(request):
+    articles = Article.objects.filter(author = request.user)
+    context = {
+        "articles":articles
+    }
+    return render(request,"dashboard.html",context)
+@login_required(login_url= "user:login")
+def addArticle(request):
+    form = ArticleForm(request.POST or None,request.FILES or None)
+    
+    if form.is_valid():
+        article = form.save(commit = False)
+        article.author = request.user
+
+        article.save()
+        messages.success(request,"Makele Başarıyla Oluşturuldu")
+        return redirect("article:dashboard")
+    return render(request,"addarticle.html",{"form":form})
+def detail(request,id):
+    #article = Article.objects.filter(id = id).first()
+    article=get_object_or_404(Article,id = id)
+    return render(request,"detail.html",{"article":article})
+@login_required(login_url= "user:login")
+def updateArticle(request,id):
+
+
+    article = get_object_or_404(Article,id=id)
+
+    form = ArticleForm(request.POST or None,request.FILES or None,instance = article)
+    if form.is_valid():
+        article = form.save(commit = False)
+        article.author = request.user
+
+        article.save()
+        messages.success(request,"Makele Başarıyla Güncellendi")
+        return redirect("article:dashboard")
+
+
+    return render(request,"update.html",{"form":form})
+@login_required
+def deleteArticle(request,id):
+    article = get_object_or_404(Article,id=id)
+
+    article.delete()
+
+    messages.success(request,"Makale Başarıyla Silindi")
+
+    return redirect("article:dashboard")
