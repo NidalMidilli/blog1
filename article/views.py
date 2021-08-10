40f9485f8e207505174ea3a607d59e7926e632c9
Nydle <nidalmidilli@gmail.com>
2021-08-10 13:00:11 +0300

    yeni

--- article/views.py
+++ article/views.py
@@ -7,6 +7,16 @@ from .models import Article
 # Create your views here.
 
 def articles(request):
+
+    keyword = request.GET.get("keyword")
+
+    if keyword:
+        articles = Article.objects.filter(title__contains = keyword)
+        return render (request,"articles.html",{"articles":articles})
+
+    articles = Article.objects.all()
+
+
     articles = Article.objects.all()
 
     return render(request,"articles.html",{"articles":articles})
