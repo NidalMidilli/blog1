a6aecd8ceefec0c87d88b6a23a2e7254b6b6ed3a
Nydle <nidalmidilli@gmail.com>
2021-08-13 12:29:41 +0300

    commit son hali

--- article/urls.py
+++ article/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "article"
 
 urlpatterns = [
-    path('dashboard/',views.dashboard,name = "dashboard"),  
+    path('dashboard/',views.dashboard,name = "dashboard"),
     path('addarticle/',views.addArticle,name = "addarticle"),
     path('article/<int:id>',views.detail,name = "detail"),
     path('update/<int:id>',views.updateArticle,name = "update"),
